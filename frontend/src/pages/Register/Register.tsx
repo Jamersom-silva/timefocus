ca53b56cadca6defc39b379b2a9410a40f3f58d9
Jamersom Silva <jamersom.silva@outlook.com>
2025-09-11 14:16:10 -0300

    criação do page registro

--- frontend/src/pages/Register/Register.tsx
+++ frontend/src/pages/Register/Register.tsx
@@ -4,7 +4,15 @@ export default function RegisterPage() {
   return (
     <div className="home-page">
       <Header />
-      <h1>Registro Page</h1>
+      <h1>Venha fazer o Cadastro </h1>
+      <h2>Para ter acesso total ao timesfocus é necessario fazer o cadastro, preenchar corretamente o formulario</h2>
+      <p>Nome</p>
+      <p>sobrenome</p>
+      <p>Cpf</p>
+      <p>telefone</p>
+      <p>E-mail</p>
+      <p>senha</p>
+      <button>Cadastre-se</button>
     </div>
   );
 }
